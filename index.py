3804ef3a7c06693de92db994be06f664f8fc73f7
digitalworld1318-sys <digitalworld1318@gmail.com>
2026-04-19 16:54:09 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-from wsgi import app
+from app import app
+from flask import jsonify
+
+@app.route('/')
+def index():
+    return jsonify({"message": "FreeFire Account Generator API", "status": "active"})
+
+if __name__ == '__main__':
+    app.run()
